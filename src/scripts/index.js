008b903cf8feb5ac9ec878d572de244aa2d1028b
Muhammad Mario <acernitro58@gmail.com>
2021-05-18 12:12:14 +0700

    menambah service worker

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,7 @@ import "regenerator-runtime";
 import "../styles/style.css";
 import "../styles/responsive.css";
 import App from "./views/app";
+import swRegister from "./utils/sw-register";
 
 const app = new App({
   button: document.querySelector("#hamburgerButton"),
@@ -15,6 +16,7 @@ window.addEventListener("hashchange", () => {
 
 window.addEventListener("load", () => {
   app.renderPage();
+  swRegister();
 });
 
 console.log("Hello Coders!");
